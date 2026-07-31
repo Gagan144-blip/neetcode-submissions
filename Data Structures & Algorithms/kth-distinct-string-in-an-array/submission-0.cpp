e4dc855a7fe06b234eed87c4cefa083fa0c3473e
Gaganpreet Kaur <gaganmt88@gmail.com>
2026-07-31 06:06:47 +0530

    Add: kth-distinct-string-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        map<string, int>mp;
+        int count = 0;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        for(int i=0;i<arr.size();i++){
+            if(mp[arr[i]] == 1){
+                count++;
+                
+            if(count == k){
+                return arr[i];
+            }
+            }
+        }
+        return "";
+    }
+};
